b490a078b503ab1698bac3b60b2a319dab96ab99
phtchung <phamthanhchung2001@gmail.com>
2023-08-05 16:41:34 +0700

    add cors to get data

--- src/main/java/com/example/gr/config/WebConfig.java
+++ src/main/java/com/example/gr/config/WebConfig.java
@@ -7,10 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-//    @Value("${appName.allowedApi}")
-//    private String myAllowedApi;
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins(myAllowedApi);
-//    }
+    @Value("${appName.allowedApi}")
+    private String myAllowedApi;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins(myAllowedApi);
+    }
 }
